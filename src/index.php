179cb91eab9ea31264048d547bad001fa0816c56
alis0nm <alisonmonteith1@aol.com>
2018-09-10 12:23:54 -0700

    Added a woohoo coz it's exciting

--- src/index.php
+++ src/index.php
@@ -16,7 +16,7 @@
         <div class="container">
             <div class="hero-unit">
                 <h1>Testing Github Webhook for automatic build</h1>
-                <h2>Congratulations</h2>
+                <h2>Congratulations - woohoo</h2>
                 <p>Your TEST PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
